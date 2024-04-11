a52716b74480f2fdc1238c65e6712c6e798df9ec
KiranDev12 <kirandev.edu@gmail.com>
2024-04-11 11:15:25 +0530

    3d model of iphone integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Navbar from "./components/Navbar";
-import Model from "./components/Model"
+import Model from "./components/Model";
 const App = () => {
-	return (
-		<main className="bg-black">
-			<Navbar />
-			<Hero />
-			<Highlights />
-			<Model />
-		</main>
-	);
+  return (
+    <main className="bg-black overflow-y-auto">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+    </main>
+  );
 };
 
 export default App;
